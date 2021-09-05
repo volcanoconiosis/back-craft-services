09048b3439cb89477fd96f281cbd77bbc73b5c2b
muntherabdlrahman <munther.abdlrahman@gmail.com>
2021-09-06 01:10:34 +0300

    basic chat is done

--- chatting/public/script.js
+++ chatting/public/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000')
+const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
--- chatting/server.js
+++ chatting/server.js
@@ -31,20 +31,20 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomName: req.params.room })
 })
 
-server.listen(8000)
+server.listen(3000)
 
 io.on('connection', socket => {
   socket.on('new-user', (room, name) => {
     socket.join(room)
     rooms[room].users[socket.id] = name
-    socket.to(room).broadcast.emit('user-connected',name)
+    socket.to(room).emit('user-connected', name)
   })
   socket.on('send-chat-message', (room, message) => {
-    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+    socket.to(room).emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
   })
   socket.on('disconnect', () => {
     getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      socket.to(room).emit('user-disconnected', rooms[room].users[socket.id])
       delete rooms[room].users[socket.id]
     })
   })
