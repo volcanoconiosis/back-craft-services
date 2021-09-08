237ffd1a225ffc3bd23d591082d7123ac5bd804d
ibrahemomari <ibrahem.omari96@gmail.com>
2021-09-08 18:30:45 +0300

    deploy

--- src/auth/routes/adminRoutes.js
+++ src/auth/routes/adminRoutes.js
@@ -2,64 +2,55 @@
 require("dotenv").config();
 const express = require("express");
 const Router = express.Router();
-const { users,
-    workerCollection,
-    clientCollection ,
-    adminCollection
+const {
+  users,
+  workerCollection,
+  clientCollection,
+  adminCollection,
 } = require("../models/index");
 const bearerAuth = require("../middlewear/bearerAuth");
 const permissions = require("../middlewear/acl");
 
-
-Router.get('/clients', bearerAuth, permissions('readAll'), getClient)
-Router.get('/getWorkers', bearerAuth, permissions('readAll'), getWorkers)
-Router.delete("/deleteuser/:id", bearerAuth, permissions("delete"), deleteUsers)
-Router.get("/users", bearerAuth, permissions("readAll"), getAllUsers)
-Router.get("/support",bearerAuth,permissions("readAll"),getSupport)
-
-
+Router.get("/clients", bearerAuth, permissions("readAll"), getClient);
+Router.get("/getWorkers", bearerAuth, permissions("readAll"), getWorkers);
+Router.delete(
+  "/deleteuser/:id",
+  bearerAuth,
+  permissions("delete"),
+  deleteUsers
+);
+Router.get("/users", bearerAuth, permissions("readAll"), getAllUsers);
+Router.get("/support", bearerAuth, permissions("readAll"), getSupport);
 
 async function getClient(req, res) {
-    let clients = await clientCollection.read()
+  let clients = await clientCollection.read();
 
-    res.status(200).send(clients)
+  res.status(200).send(clients);
 }
-
-
-
-
 
 async function getWorkers(req, res) {
-    let workers = await workerCollection.read()
-    res.status(200).send(workers)
-
+  let workers = await workerCollection.read();
+  res.status(200).send(workers);
 }
-
 
 async function deleteUsers(req, res) {
-    const id = req.params.id;
-    await users.destroy({ where: { id: id } });
-    const userRecords = await users.findAll({});
-    const list = await userRecords.map((user) => user);
-    res.send(list);
+  const id = req.params.id;
+  await users.destroy({ where: { id: id } });
+  const userRecords = await users.findAll({});
+  const list = await userRecords.map((user) => user);
+  res.send(list);
 }
-
-
 
 async function getAllUsers(req, res) {
-    const userRecords = await users.findAll({});
-    const list = userRecords.map((user) => user);
-    res.status(200).json(list);
+  const userRecords = await users.findAll({});
+  const list = userRecords.map((user) => user);
+  res.status(200).json(list);
 }
 
-
-async function getSupport(req,res){
-    const userRecords = await adminCollection.read();
-    const list = userRecords.map((user) => user);
-    res.status(200).json(list);
+async function getSupport(req, res) {
+  const userRecords = await adminCollection.read();
+  const list = userRecords.map((user) => user);
+  res.status(200).json(list);
 }
 
-
-
-
-module.exports = Router
+module.exports = Router;
